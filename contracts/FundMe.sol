bfeb4148a03c770f7577e6bbd2f80cfe66240eba
akrabulislam <akrabulislam1@gmail.com>
2022-06-30 19:48:31 +0600

    added basic smart contract

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -8,6 +8,7 @@ contract FundMe {
       //How do we send ETH in this contract
       require(msg.value >= 1e18,"Don't send enough");
       //What is reverting?
+      //What is reverting?
       //Undo any action before and send remaining gas back
     }
 }
